aba0c9001c7bac0df8400e16d4317f94347ad026
irem nur ceylan <ceylannnirem@outlook.com>
2021-02-22 01:36:51 +0300

    03.02.2021 Ders Sonu

--- Entities/Concrete/Category.cs
+++ Entities/Concrete/Category.cs
@@ -6,8 +6,8 @@ using System.Text;
 namespace Entities.Concrete
 { 
     // Çıplak Class Kalmasın
-   public class Category : IEntity 
-    {
+   public class Category : IEntity
+    { 
         public int CategoryId { get; set; }
         public string CategoryName { get; set; }
 
