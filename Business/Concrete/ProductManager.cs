56753fd4a787aa98a230ee493316de2ff279a6ac
irem nur ceylan <ceylannnirem@outlook.com>
2021-03-02 18:05:33 +0300

    17 Şubat Ders Sonu

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -1,12 +1,17 @@
 ﻿using Business.Abstract;
 using Business.Constants;
+using Business.ValidationRules.FluentValidation;
+using Core.Aspects.Autofac.Validation;
+using Core.CrossCuttingConcerns.Validation;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
 using DataAccess.Concrete.InMemory;
 using Entities.Concrete;
 using Entities.DTOs;
+using FluentValidation;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace Business.Concrete
@@ -20,14 +25,12 @@ namespace Business.Concrete
             _productDal = productDal;
         }
 
+        [ValidationAspect(typeof(ProductValidator))]
         public IResult Add(Product product)
         {
             // İş kodları buraya yazılır
-            if (product.ProductName.Length < 2)
-            {
-                // magic strings
-                return new ErrorResult(Messages.ProductNameInvalid);
-            }
+            // validation -- dogrulama kodları
+
             _productDal.Add(product);
             return new SuccessResult(Messages.ProductAdded);
         }
